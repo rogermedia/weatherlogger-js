0ef317d0aa4bfb22bde7e4cb99ac891188dfe6b8
Roger Williams <roger@rogerwilliamsmedia.com>
2022-05-25 17:42:50 -0600

    Update config.ts imperial units and mancos local

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const unit = process.env.UNIT ?? 'metric'
-export const location = process.env.WEATHER_LOCATION ?? 'paris'
+export const unit = process.env.UNIT ?? 'imperial'
+export const location = process.env.WEATHER_LOCATION ?? 'mancos'
 export const dbType = process.env.DB_TYPE ?? 'mysql'
 export const frequency = process.env.FREQUENCY ?? 3
